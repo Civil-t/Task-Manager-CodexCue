9b2908573246942ffee7ed3120d96b4f804cc6a0
Tapiwa <tapiwachimbwanda4@gmail.com>
2024-07-03 04:54:11 -0400

    finished dashboard grid setup

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,7 +4,16 @@ import "./Dashboard.css";
 function Dashboard() {
   return (
     <>
-      <div className="dash-container">Dashboard</div>
+      <div className="dash-container">
+        <div id="navbar">Navbar</div>
+        <div id="body">
+          <div id="main-section">
+            <div id="dash-info">Dashboard Info</div>
+            <div id="projects-info">Projects</div>
+          </div>
+          <div id="side-section">Side Section</div>
+        </div>
+      </div>
     </>
   );
 }
